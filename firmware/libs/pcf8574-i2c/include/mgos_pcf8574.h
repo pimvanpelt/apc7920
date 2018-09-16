60771d5a9721a0e0ae2a158db17807c565ff87aa
Pim van Pelt <pim@google.com>
2018-09-16 20:56:33 +0200

    Add implementation of PCF8574 based on datasheet - only one register (read from reg 0x01, write 1 byte)

--- firmware/libs/pcf8574-i2c/include/mgos_pcf8574.h
+++ firmware/libs/pcf8574-i2c/include/mgos_pcf8574.h
@@ -2,9 +2,17 @@
 
 #include "mgos.h"
 #include "mgos_i2c.h"
+#include "mgos_gpio.h"
 
 struct mgos_pcf8574;
 
 struct mgos_pcf8574 *mgos_pcf8574_create(struct mgos_i2c *i2c, uint8_t i2c_addr);
 void mgos_pcf8574_destroy(struct mgos_pcf8574 **dev);
+
+/* Copied the style from mgos_gpio.h */
+bool mgos_pcf8574_set_mode(struct mgos_pcf8574 *dev, int pin, enum mgos_gpio_mode mode);
+bool mgos_pcf8574_read(struct mgos_pcf8574 *dev, int pin);
+void mgos_pcf8574_write(struct mgos_pcf8574 *dev, int pin, bool level);
+bool mgos_pcf8574_toggle(struct mgos_pcf8574 *dev, int pin);
+bool mgos_pcf8574_read_out(struct mgos_pcf8574 *dev, int pin);
 
--- firmware/libs/pcf8574-i2c/src/mgos_pcf8574_internal.h
+++ firmware/libs/pcf8574-i2c/src/mgos_pcf8574_internal.h
@@ -7,4 +7,13 @@
 struct mgos_pcf8574 {
   uint8_t i2c_addr;
   struct mgos_i2c *i2c;
+
+  /* bits of output values */
+  uint8_t _output;
+
+  /* bits of input values */
+  uint8_t _input;
+
+  /* directionality (output or input) */
+  uint8_t _mode;
 };
