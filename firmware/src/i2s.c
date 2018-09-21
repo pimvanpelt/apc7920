5bb3ce627eb4f08d1d63d12eadc99d37b51d2a1a
Pim van Pelt <pim@google.com>
2018-09-21 21:25:04 +0200

    New monitor all 8 ADC channels! Grab some stats per channel per measurement cycle

--- firmware/src/i2s.c
+++ firmware/src/i2s.c
@@ -10,33 +10,64 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 
-#define SAMPLE_RATE    (4000)
+#define SAMPLE_RATE    (9000)
 #define I2S_NUM        (0)
+
+struct adc_chan {
+  uint32_t sum;
+  uint16_t count;
+  uint16_t min;
+  uint16_t min_at;
+  uint16_t max;
+  uint16_t max_at;
+};
+
 
 static QueueHandle_t s_i2s_event_queue;
 
 void i2s_scanner(void *pvParams) {
-  int i2s_read_len = 1024 * 2;
+  int      i2s_read_len = 1024 * 2;
   uint16_t i2s_read_buff[1024];
 
-  while(1) {
+  while (1) {
     system_event_t evt;
     if (xQueueReceive(s_i2s_event_queue, &evt, portMAX_DELAY) == pdPASS) {
-      if (evt.event_id==2) { // I2S_EVENT_RX_DONE
-        size_t bytes_read = 0;
-	uint32_t samples[16];
+      if (evt.event_id == 2) { // I2S_EVENT_RX_DONE
+        size_t          bytes_read = 0;
+        struct adc_chan s_adc_chan[8];
 
-        i2s_read(I2S_NUM, (char*)i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
+        i2s_read(I2S_NUM, (char *)i2s_read_buff, i2s_read_len, &bytes_read, portMAX_DELAY);
 //        LOG(LL_INFO, ("Received %u bytes", bytes_read));
-	for (int i=0; i<16; i++)
-	  samples[i]=0;
 
-	for (int i=0; i < 1024; i++)
-	  samples[i2s_read_buff[i]>>12]++;
-	
-        LOG(LL_INFO, ("%u %u %u %u", samples[4], samples[5], samples[6], samples[7]));
+        memset(s_adc_chan, 0, sizeof(s_adc_chan));
+        for (int i = 0; i < 8; i++) {
+          s_adc_chan[i].min = 0xfff;
+        }
+        for (int i = 0; i < bytes_read / 2; i++) {
+          uint8_t  chan      = (i2s_read_buff[i] >> 12) & 0x07;
+          uint16_t adc_value = i2s_read_buff[i] & 0xfff;
+          s_adc_chan[chan].count++;
+          s_adc_chan[chan].sum += adc_value;
+          if (adc_value < s_adc_chan[chan].min) {
+            s_adc_chan[chan].min    = adc_value;
+            s_adc_chan[chan].min_at = i;
+          }
+          if (adc_value > s_adc_chan[chan].max) {
+            s_adc_chan[chan].max    = adc_value;
+            s_adc_chan[chan].max_at = i;
+          }
+        }
+        for (int i = 0; i < 8; i++) {
+          LOG(LL_INFO, ("chan: %d min: %u min_at: %u max: %u max_at: %u count=%u avg=%.f",
+                        i, s_adc_chan[i].min,
+                        s_adc_chan[i].min_at,
+                        s_adc_chan[i].max,
+                        s_adc_chan[i].max_at,
+                        s_adc_chan[i].count,
+                        ((float)s_adc_chan[i].sum) / s_adc_chan[i].count));
+        }
 
-        ESP_LOG_BUFFER_HEX("buf", i2s_read_buff, 32);
+//        ESP_LOG_BUFFER_HEX("buf", i2s_read_buff+1024-64, 64);
       } else {
         LOG(LL_ERROR, ("Event %d received", evt.event_id));
       }
@@ -45,7 +76,7 @@ void i2s_scanner(void *pvParams) {
 }
 
 void i2s_init() {
-  i2s_config_t     i2s_config = {
+  i2s_config_t i2s_config = {
     .mode                 = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
     .sample_rate          = SAMPLE_RATE,
     .bits_per_sample      = 16,
@@ -66,22 +97,20 @@ void i2s_init() {
   // [7:4] Channel
   // [3:2] Bit Width; 3=12bit, 2=11bit, 1=10bit, 0=9bit
   // [1:0] Attenuation; 3=11dB, 2=6dB, 1=2.5dB, 0=0dB
-  // 0x47 == Chan4, 12bit, 11dB
-  // 0x57 == Chan5, 12bit, 11dB
-  WRITE_PERI_REG(SYSCON_SARADC_SAR1_PATT_TAB1_REG, 0x4F5F6F7F);
-//  WRITE_PERI_REG(SYSCON_SARADC_SAR1_PATT_TAB2_REG, 0x00000000);
+  WRITE_PERI_REG(SYSCON_SARADC_SAR1_PATT_TAB1_REG, 0x0F1F2F3F);
+  WRITE_PERI_REG(SYSCON_SARADC_SAR1_PATT_TAB2_REG, 0x4F5F6F7F);
 
-  // Scan 2 channels.
-  SET_PERI_REG_BITS(SYSCON_SARADC_CTRL_REG, SYSCON_SARADC_SAR1_PATT_LEN, 3, SYSCON_SARADC_SAR1_PATT_LEN_S);
+  // Scan multiple channels.
+  SET_PERI_REG_BITS(SYSCON_SARADC_CTRL_REG, SYSCON_SARADC_SAR1_PATT_LEN, 7, SYSCON_SARADC_SAR1_PATT_LEN_S);
 
   // The raw ADC data is written to DMA in inverted form. Invert back.
   SET_PERI_REG_MASK(SYSCON_SARADC_CTRL2_REG, SYSCON_SARADC_SAR1_INV);
 
   // It is necessary to add a delay before switching on the ADC for a reliable start (?)
   // WiFi seems to mess with ADC1 otherwise...
-  vTaskDelay(8000/portTICK_RATE_MS);
+  vTaskDelay(8000 / portTICK_RATE_MS);
   i2s_adc_enable(I2S_NUM);
 
   // Start the receiver thread
-  xTaskCreate(i2s_scanner, "i2s_scanner", 1024 * 8, NULL, 10, NULL);
+  xTaskCreate(i2s_scanner, "i2s_scanner", 1024 * 10, NULL, 10, NULL);
 }
